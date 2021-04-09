d539cf86fc5144596d108ec750f9270a3cbc001e
adamus.r <adamus.r@gmail.com>
2021-04-09 09:47:32 +0200

    book keeper test class added

---
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -0,0 +1,20 @@
+package pl.com.bottega.ecommerce.sales.domain.invoicing;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BookKeeperTest {
+
+    @BeforeEach
+    void setUp() throws Exception {
+
+    }
+
+    @Test
+    void test() {
+        fail("not implemented");
+    }
+
+}
